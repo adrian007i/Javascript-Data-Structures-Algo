21b704b9a4a9105fc3ea4d79b2d0cec879b32062
Adrian John <adrianjohn1993@gmail.com>
2022-03-04 17:55:11 -0400

    max subarray leetcode

---
+++ problems/leetcode/maximum-subarray.js
@@ -0,0 +1,21 @@
+var maxSubArray = function(nums) {
+    
+    let max = nums[0];
+    let sum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) { 
+
+        if(nums[i] < sum + nums[i]) { 
+            sum = sum + nums[i];
+        }
+        else sum = nums[i];
+
+        if(sum > max) max = sum;  
+
+        console.log(sum);
+    }
+
+    return max; 
+};
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
